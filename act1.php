e6cb19a1a6b8efaa61202491c4dce79c5b591b52
pogi2005 <paulsantiago296@gmail.com>
2025-09-03 09:15:46 +0000

    PHP Arrays and Booleans

--- act1.php
+++ act1.php
@@ -1,13 +1,36 @@
 <?php
-    $productCatalog = (
-        "Electronics" => array (
-            "Laptop" => array(
+$name = ["Laptop","Smartphone","USB drive", "wireless mouse","wireless headset"];
+echo "Product Names: " . implode(", ", $productNames) . "\n";   
+    $productNames = array[
+        "Electronics" => array [
+            "Laptop" => array[
                  "name" => "Laptop",
                   "description" => "High-performance laptop.",
-                  "price" => "9000.",
-            )
-        ),
-    );
+                  "price" => 9000.,
+            ],
+            "Smartphone" => array[
+                "name" => "Tecno Camon 30s",
+                "description" => "High-performance laptop.",
+                "price" => 8000.,
+            ],
+            "USB" => array[
+                "name" => "USB drive",
+                "description" => "Malaki yung space niya.",
+                "price" => 299.,
+            ],
+            "wireless mouse" => array[
+                "name" => "wireless mouse",
+                "description" => "marerelax kamay mo habang nag lalaro.",
+                "price" => 599.,
+            ],
+            "wireless headset" => array[
+                "name" => "wireless headset",
+                "description" => "malakas mabibingi ka sa lakas.",
+                "price" => 900,
+            ],
 
+        ],
+    ];
+    
 
 ?>
