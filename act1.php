f8ebaf8fd57b13bd2daffb97b6791c6ed897c8a2
pogi2005 <paulsantiago296@gmail.com>
2025-09-03 08:04:13 +0000

    PHP Arrays and Booleans

---
+++ act1.php
@@ -0,0 +1,13 @@
+<?php
+    $productCatalog = (
+        "Electronics" => array (
+            "Laptop" => array(
+                 "name" => "Laptop",
+                  "description" => "High-performance laptop.",
+                  "price" => "9000.",
+            )
+        ),
+    );
+
+
+?>
